b3c190f502dceabf1c3fa8cf48a26fa0e3b4a7ce
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-02-08 20:38:32 -0600

    what happens to health when robots are attacked

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,19 +1,44 @@
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
 //Note the lack of quotation marks around playerName.
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
+console.log(playerName, playerAttack, playerHealth);
 
-// this will do math and log 20
-console.log(10 + 10);
-
-// what is this?
-console.log("Our robot's name is " + playerName);
+//String data type variable
+var enemyName = "Roborto";
+//Number data type variable
+var enemyHealth = 50;
+var enemyAttack = 12;
 
 // this creates a function named "fight"
 //Think of an alert as a way to get information to the user-not the developer.
-function fight(){
-    window.alert("The fight has begun!");
-}
+//this is a function expression. If it didnt have the (var fight =) it would be a function declaration
+var fight = function() {
+    // Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+    
+    //Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use the result to update the value in the 'enemyHealth' variable
+    enemyHealth = enemyHealth - playerAttack;
 
+    // Log a resulting message to the console so we know that it worked.
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+
+    // Subtract the value of 'enemyAttack' from the value of the 'playerHealth' and use that result to update the value in the 'playerHealth' variable
+    playerHealth = playerHealth - enemyAttack;
+
+    // Log a resulting message to the console so we know that it worked
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+};
+//execute function
 //fight();
+// this will do math and log 20
+//console.log(10 + 10);
+
+// this will combine the results for playerName and the starting of sentence of "Our robot's name is"
+// notice the space between is and "
+//console.log("Our robot's name is " + playerName);
